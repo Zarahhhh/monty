423252a879afd4407df7f92fb3ec1e44c1f14591
Zarahhhh <zakariyyahrahmat@gmail.com>
2024-09-27 02:35:03 +0100

    0x19. C - Stacks, Queues - LIFO, FIFO

--- main.c
+++ main.c
@@ -1,35 +1,108 @@
 #include "monty.h"
+#include <stdio.h>
 
-glob_t glob;
+global_t vglo;
 
 /**
- * main - The Main function for monty program.
- * @argc: arguments count
+ * free_vglo - frees the global variables
+ * Return: no return
+ */
+
+void free_vglo(void)
+
+{
+	free_dlistint(vglo.head);
+	free(vglo.buffer);
+	fclose(vglo.fd);
+}
+
+/**
+ * start_vglo - initializes the global variables
+ * @fd: file descriptor
+ * Return: no return
+ */
+
+void start_vglo(FILE *fd)
+
+{
+	vglo.lifo = 1;
+	vglo.cont = 1;
+	vglo.arg = NULL;
+	vglo.head = NULL;
+	vglo.fd = fd;
+	vglo.buffer = NULL;
+}
+
+/**
+ * check_input - checks if the file exists and if the file can
+ * be opened
+ * @argc: argument count
  * @argv: argument vector
- * Return: 0 on success, non-zero on failure
+ * Return: file struct
+ */
+
+FILE *check_input(int argc, char *argv[])
+
+{
+	FILE *fd;
+
+	if (argc == 1 || argc > 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(argv[1], "r");
+
+	if (fd == NULL)
+	{
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	return (fd);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 
 {
-	stack_t *queues = NULL;
+	void (*f)(stack_t **stack, unsigned int line_number);
+	FILE *fd;
+	size_t size = 256;
+	ssize_t nlines = 0;
+	char *lines[2] = {NULL, NULL};
 
-	if (argc != 2)
+	fd = check_input(argc, argv);
+	start_vglo(fd);
+	nlines = getline(&vglo.buffer, &size, fd);
+	while (nlines != -1)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		lines[0] = _strtoky(vglo.buffer, " \t\n");
+		if (lines[0] && lines[0][0] != '#')
+		{
+			f = get_opcodes(lines[0]);
+			if (!f)
+			{
+				dprintf(2, "L%u: ", vglo.cont);
+				dprintf(2, "unknown instruction %s\n", lines[0]);
+				free_vglo();
+				exit(EXIT_FAILURE);
+			}
+			vglo.arg = _strtoky(NULL, " \t\n");
+			f(&vglo.head, vglo.cont);
+		}
+		nlines = getline(&vglo.buffer, &size, fd);
+		vglo.cont++;
 	}
 
-	glob.file = fopen(argv[1], "r");
-	if (glob.file == NULL)
-	{
-		fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+	free_vglo();
 
-	process_line(&queues);
-	fclose(glob.file);
-	free(glob.line);
-	free_list(queues);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
--- monty.h
+++ monty.h
@@ -1,90 +1,112 @@
-#ifndef MONTY_H
-#define MONTY_H
-#define _GNU_SOURCE
+#ifndef MONTY
+#define MONTY
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
-#include <sys/types.h>
-#include <stdio.h>
-
 
 /**
- * struct global_s - variables -> args, file, line content
- * @arg: value
- * @line: input line content
- * @file: pointer to monty file
- * Description: variables that carries values through the program
- */
-
-typedef struct global_s
-
-{
-	char *arg;
-	FILE *file;
-	char *line;
-} glob_t;
-
-extern glob_t glob;
-
-/**
- * struct queues_s - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- * Description: doubly linked list node structure for stack, queues, LIFO, FIFO
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 
-typedef struct queues_s
+typedef struct stack_s
 
 {
 	int n;
-	struct queues_s *prev;
-	struct queues_s *next;
-} queues_t;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct globals - global structure to use in the functions
+ * @lifo: is stack or queue
+ * @cont: current line
+ * @arg: second parameter inside the current line
+ * @head: doubly linked list
+ * @fd: file descriptor
+ * @buffer: input text
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+
+typedef struct globals
+
+{
+	int lifo;
+	unsigned int cont;
+	char  *arg;
+	stack_t *head;
+	FILE *fd;
+	char *buffer;
+} global_t;
 
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 
 typedef struct instruction_s
 
 {
 	char *opcode;
-	void (*f)(stack_t **queues, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Function prototypes */
+extern global_t vglo;
 
-void process_line(stack_t **stack);
-void exec_opcode(char *opcode, stack_t **stack, unsigned int line_number);
+/* opcode_instructuions*/
 
-/* Stack helper functions */
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **doubly, unsigned int cline);
+void _pop(stack_t **doubly, unsigned int cline);
+void _swap(stack_t **doubly, unsigned int cline);
+void _queue(stack_t **doubly, unsigned int cline);
+void _stack(stack_t **doubly, unsigned int cline);
+void _add(stack_t **doubly, unsigned int cline);
+void _nop(stack_t **doubly, unsigned int cline);
+void _sub(stack_t **doubly, unsigned int cline);
+void _div(stack_t **doubly, unsigned int cline);
+void _mul(stack_t **doubly, unsigned int cline);
+void _mod(stack_t **doubly, unsigned int cline);
+void _pchar(stack_t **doubly, unsigned int cline);
+void _pstr(stack_t **doubly, unsigned int cline);
+void _rotl(stack_t **doubly, unsigned int cline);
+void _rotr(stack_t **doubly, unsigned int cline);
 
-stack_t *add_nodeint(stack_t **stack, const int h);
-int is_integer(char *str);
-void free_list(stack_t *stack);
+/*get function*/
 
-void op_nothing(stack_t **stack, unsigned int line_number);
-void _sub(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
-void _mul(stack_t **stack, unsigned int line_number);
-void _mod(stack_t **stack, unsigned int line_number);
-void print_char(stack_t **stack, unsigned int line_number);
-void print_str(stack_t **stack, unsigned int line_number);
-void push_opcode(stack_t **stack, unsigned int line_number);
-void op_pall(stack_t **stack, unsigned int line_number);
-void print_int(stack_t **stack, unsigned int line_number);
-void pop_opcode(stack_t **stack, unsigned int line_number);
-void swap_top(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
 
+/*imported functions*/
 
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _strcmp(char *s1, char *s2);
 
-#endif /* monty.h */
+/* doubly linked list functions */
+
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+stack_t *add_dnodeint(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
+
+/* main */
+
+void free_vglo(void);
+
+#endif
